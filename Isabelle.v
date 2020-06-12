63b93eed27d1c304d357506874338fa8abdebcdd
Georgy Dunaev <georgedunaev@gmail.com>
2020-06-12 20:55:40 +0300

    add Typing function

--- Isabelle.v
+++ Isabelle.v
@@ -1,5 +1,5 @@
 (* Verification of Isabelle's FOL library
-1)
+1) Maybe http://color.inria.fr/papers/koprowski06draft.pdf ?
 *)
 (* use Isabelle notation in comments *)
 Inductive Types : Set :=
@@ -9,6 +9,17 @@ Inductive Types : Set :=
 | TO : Types (* "o"=bool=object logic propositions *)
 | TI : Types (* "i"=individual objects -- from ZF_base.thy *)
 .
+
+
+Fixpoint eqTypes (a b:Types) : bool :=
+match a, b with
+| TVar an, TVar bn => Nat.eqb an bn
+| TImp AT1 AT2, TImp BT1 BT2 => andb (eqTypes AT1 BT1) (eqTypes AT2 BT2)
+| TProp, TProp => true
+| TO, TO => true
+| TI, TI => true
+| _, _ => false
+end.
 
 Check Types.
 
@@ -32,13 +43,52 @@ match c with
 end.
 
 Inductive Terms : Set :=
-| TVar : nat -> Terms (* x0 x1 x2 ... *)
-| TCon : PT -> Terms
-| TLam : nat -> Types -> Terms -> Terms
-| TApp : Terms -> Terms -> Terms
+| EVar : nat -> Terms (* x0 x1 x2 ... *)
+| ECon : PT -> Terms
+| ELam : nat -> Types -> Terms -> Terms
+| EApp : Terms -> Terms -> Terms
 .
 
+Section Interpretation.
+Context (I:nat->Types).
+(*
+Fixpoint TypeOfTerm (t:Terms) : Types :=
+match t with
+| EVar n => I n
+| ECon c => PTT c
+| ELam n T t => TImp T (TypeOfTerm t)
+| EApp t1 t2 => ?
+end.
+*)
 
+(* Inductive type has decidable equality iff
+its constructors has arguments of types with decidable equality only. *)
+
+
+Fixpoint TypeOfTerm (t:Terms) : option Types :=
+match t with
+| EVar n => Some (I n)
+| ECon c => Some (PTT c)
+| ELam n T t => 
+  match (TypeOfTerm t) with
+  | Some T2 => Some (TImp T T2)
+  | None => None
+  end
+| EApp t1 t2 =>
+  match (TypeOfTerm t1) with
+  | Some T1 =>
+    match T1 with
+    | TImp TA TB =>
+      match (TypeOfTerm t2) with
+      | Some T2 => if eqTypes T2 TA then Some TB else None
+      | _ => None
+      end
+    | _ => None
+    end
+  | None => None
+  end
+end.
+End Interpretation.
 
 Section Theory.
 Context (A:Set) (TCons : A -> Types)
