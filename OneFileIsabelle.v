84e3fbad2b708d251b398210330c69995e0d677b
Georgy Dunaev <georgedunaev@gmail.com>
2020-08-17 16:36:05 +0300

    add

--- OneFileIsabelle.v
+++ OneFileIsabelle.v
@@ -6,16 +6,16 @@ Context (typeconstants:Set).
 Context (typeval : typeconstants -> nat).
 
 (* Notation: tau, sigma, alpha *)
-Inductive Types : Set :=
-| TVar : nat -> Types (* 'a 'b 'c ... *)
+Inductive Types : Type :=
+| TVar : nat -> Types (* "'a 'b 'c ..." *)
 | TCon (tc : typeconstants) : (Vector.t Types (typeval tc)) -> Types
-| TImp : Types -> Types -> Types (* "=>" *)
+| TImp : Types -> Types -> Types (* "=>" !! should be part of typeconstants !! *)
 .
 
 Definition idsubst : nat -> Types
  := fun (n:nat) => TVar n.
 
-Definition dosubst (sub:nat -> Types) (T : Types) : Types
+Definition dosubst (subst : nat -> Types) (T : Types) : Types
  := T. (* TODO *)
 
 Context (termconstants : Set).
@@ -23,24 +23,37 @@ Context (termconstants : Set).
 
 (* Notation: t, u, P, Q *)
 Inductive Terms : Set :=
-| EVar : nat -> Terms (* x0 x1 x2 ... *)
-| ECon (tc : termconstants) (subst : nat -> Types) : Terms (* c_{[tau_n/alpha_n]} *)
+| EVar : nat -> Terms (* "x0 x1 x2 ... " *)
+| ECon (tc : termconstants) (subst : nat -> Types) : Terms (* "c_{[tau_n/alpha_n]}" *)
 | EApp : Terms -> Terms -> Terms  (* "t u" *)
-| ELam : nat -> Types -> Terms -> Terms (* lambda x_n :: tau. t *)
+| ELam : nat -> Types -> Terms -> Terms (* "lambda x_n :: tau. t" *)
 .
 (* | ECon : PT -> Terms *)
 
+Context (Sigma : termconstants -> Types).
+(* Context (TImp : typeconstants). *)
+
+(* Terms' Prf predicate. *)
+Section TPrf_sec.
+Context (G : nat -> Types).
+Inductive TPrf :  Terms -> Types -> Type :=
+| PEHyp (xn: nat) (tau : Types) : TPrf (EVar xn) (G xn)
+| PEConType (c : termconstants) (subst : nat -> Types) :
+    TPrf (ECon c subst) (dosubst subst (Sigma c))
+| PEApp t u tau sigma : TPrf t (TImp tau sigma) -> TPrf u tau -> TPrf (EApp t u) sigma
+.
+End TPrf_sec.
 Context (proofconstants:Set).
 (* Context (proofval : proofconstants -> nat). *)
 
 (* Notation: p, q *)
 Inductive Proofs : Set :=
 | PVar : nat -> Proofs (* h0 h1 h2 ... *)
-| PCon (tc : proofconstants) (subst : nat -> Types) : Proofs (* c_{[tau_n/alpha_n]} *)
+| PCon (tc : proofconstants) (subst : nat -> Types) : Proofs (* "c_{[tau_n/alpha_n]}" *)
 | PApp1 : Proofs -> Terms -> Proofs  (* "p . t" *)
 | PApp2 : Proofs -> Proofs -> Proofs  (* "p * t" *)
-| PLam1 : nat -> Types -> Proofs -> Proofs (* llambda x_n :: T. p *)
-| PLam2 : nat -> Types -> Proofs -> Proofs (* llambda h_n :: T. p *)
+| PLam1 : nat -> Types -> Proofs -> Proofs (* "llambda x_n :: T. p" *)
+| PLam2 : nat -> Types -> Proofs -> Proofs (* "llambda h_n :: T. p" *)
 .
 
 
